cbcb5737917f12d5983add34dc85e86a88efe329
Faylun <kauan.katysuky@gmail.com>
2023-08-30 19:50:10 -0300

    commit no projeto Namoro

---
+++ Projeto/Principal/Principal.java
@@ -0,0 +1,13 @@
+package Principal;
+
+import View.TelaNamoro;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        TelaNamoro tela = new TelaNamoro();
+        tela.setVisible(true);
+    }
+    
+}
